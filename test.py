1722fbbd6ae6fb76ad1e3bec15fc5154de146d94
pygineer2023 <pygineer2023@gmail.com>
2023-05-10 16:43:07 +0900

    test

--- test.py
+++ test.py
@@ -1,53 +1,71 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from azure.storage.datalake import DataLakeServiceClient
+from azure.synapse.spark import SparkBatchJob, SparkClient
 import logging
-import os
 import zipfile
+import io
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def process_zip_file():
-    try:
-        # リクエストからZIPファイルを取得
-        file = request.files['file']
-        
-        # ZIPファイルを一時的なディレクトリに保存
-        temp_dir = '/tmp'
-        zip_path = os.path.join(temp_dir, file.filename)
-        file.save(zip_path)
-        
-        # ZIPファイルを展開
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(temp_dir)
-        
-        # TODO: 受け取ったファイルのチェックを実装
-        
-        # ログに受け取ったファイルの情報を表示
-        logging.info('Received files:')
-        for root, _, files in os.walk(temp_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                logging.info(file_path)
-        
-        # TODO: システム割り当てマネージドIDを利用してキーボルトから情報を取得
-        
-        # TODO: ユーザー割り当てマネージドIDを利用してAzure Storageにファイルを配置
-        
-        # TODO: ユーザー割り当てマネージドIDを利用してdotDataを使って予測用モデルを作成
-        
-        # TODO: 予測を実行
-        
-        # 予測結果を返却
-        return 'Prediction result'
-        
-    except Exception as e:
-        # エラーハンドリング
-        logging.error(str(e))
-        return 'Error occurred', 500
+# Azure Key Vault
+credential = DefaultAzureCredential()
+key_vault_client = SecretClient(vault_url="https://<your-key-vault-name>.vault.azure.net", credential=credential)
 
-if __name__ == '__main__':
-    # ログ設定
-    logging.basicConfig(level=logging.INFO)
-    
-    # アプリケーションの起動
-    app.run()
+# Azure Synapse Spark
+synapse_workspace_endpoint = "https://<your-synapse-workspace-name>.dev.azuresynapse.net"
+spark_client = SparkClient(synapse_workspace_endpoint, credential)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        # Check files in the received ZIP
+        if 'file' not in request.files:
+            return jsonify({'message': 'No file part', 'status': 'error'})
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'message': 'No selected file', 'status': 'error'})
+        if file and zipfile.is_zipfile(file):
+            zip = zipfile.ZipFile(io.BytesIO(file.read()))
+            for name in zip.namelist():
+                if not (name.endswith('.csv') or name.endswith('.json')):
+                    return jsonify({'message': 'Invalid file type in zip', 'status': 'error'})
+
+        # Access secrets from Key Vault
+        storage_account_name = key_vault_client.get_secret("<your-storage-account-name-secret-name>")
+        storage_account_key = key_vault_client.get_secret("<your-storage-account-key-secret-name>")
+        
+        # Use user-assigned Managed ID to place the received CSV files in a container in Azure Data Lake Storage
+        data_lake_service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name.value), credential=credential)
+        filesystem_client = data_lake_service_client.get_file_system_client(file_system="<your-file-system-name>")
+        directory_client = filesystem_client.get_directory_client("<your-directory-name>")
+        for name in zip.namelist():
+            if name.endswith('.csv'):
+                file_client = directory_client.get_file_client(name)
+                file_client.upload_data(zip.read(name), overwrite=True)
+
+        # Use user-assigned Managed ID to create a prediction model using dotData placed in Synapse Workspace's Spark pool
+        # Configure your Spark job here...
+        # ...
+
+        logging.info('Prediction results...')  # replace with actual log message
+
+        return jsonify({
+            'message': 'Prediction results...',  # replace with actual prediction results
+            'status': 'success'
+        })
+    except Exception as e:
+        # Error handling
+        logging.error('An error occurred: %s', str(e))
+
+        return jsonify({
+            'message': str(e),
+            'status': 'error'
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True)
